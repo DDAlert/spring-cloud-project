a30f479f778e0b0a5799f595574f0632dac75537
daichuanhao <daichuanhao@bamboocloud.cn>
2020-11-15 16:31:38 +0800

    zuul的路由重试，引入retry依赖

--- spring-cloud-producer2/src/main/java/com/example/springcloudproducer2/HelloController.java
+++ spring-cloud-producer2/src/main/java/com/example/springcloudproducer2/HelloController.java
@@ -4,10 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
+
 @RestController
 public class HelloController {
+    private static Logger logger = Logger.getLogger(String.valueOf(HelloController.class));
     @RequestMapping("/hello")
     public String index(@RequestParam String name){
+        logger.info("request two name is " + name);
+        try {
+            Thread.sleep(1000000);
+        }catch (Exception e){
+            logger.info("hello two error" + e);
+        }
         return "hello "+name+",this is producer2 first message";
     }
 }
